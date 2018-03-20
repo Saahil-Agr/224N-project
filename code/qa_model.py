7d893084d1929cc5cd50da7e87efeebfebd30ab0
saahil <saahil@stanford.edu>
2018-03-20 10:21:32 -0700

    removed the import of BiLSTM2 from qa_model

--- code/qa_model.py
+++ code/qa_model.py
@@ -30,7 +30,7 @@ from tensorflow.python.ops import embedding_ops
 from evaluate import exact_match_score, f1_score
 from data_batcher import get_batch_generator
 from pretty_print import print_example
-from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, BiDaff, BiLSTM,BiLSTM2
+from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, BiDaff, BiLSTM
 
 logging.basicConfig(level=logging.INFO)
 
