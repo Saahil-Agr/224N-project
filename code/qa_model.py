7457f7d7fa0f9ff4c49edd94b871a84b75add589
saahil <saahil@stanford.edu>
2018-03-20 15:25:33 -0700

    one more mistake

--- code/qa_model.py
+++ code/qa_model.py
@@ -516,20 +516,22 @@ class QAModel(object):
                 # Calc interesting things
                 true_answer_length = len(true_answer)
                 pred_answer_length = len(normalize_answer(pred_answer))
-                if "where" in batch.qn_tokens or "Where" in batch.qn_tokens[ex_idx]:
+                if "where" in batch.qn_tokens[ex_idx] or "Where" in batch.qn_tokens[ex_idx]:
                     question_type = "where"
-                elif "when" in batch.qn_tokens or "When" in batch.qn_tokens[ex_idx]:
+                elif "when" in batch.qn_tokens[ex_idx] or "When" in batch.qn_tokens[ex_idx]:
                     question_type = "when"
-                elif "how" in batch.qn_tokens or "how" in batch.qn_tokens[ex_idx]:
+                elif "how" in batch.qn_tokens[ex_idx] or "how" in batch.qn_tokens[ex_idx]:
                     question_type = "how"
-                elif "why" in batch.qn_tokens or "Why" in batch.qn_tokens[ex_idx]:
+                elif "why" in batch.qn_tokens[ex_idx] or "Why" in batch.qn_tokens[ex_idx]:
                     question_type = "why"
-                elif "what" in batch.qn_tokens or "What" in batch.qn_tokens[ex_idx]:
+                elif "what" in batch.qn_tokens[ex_idx] or "What" in batch.qn_tokens[ex_idx]:
                     question_type = "what"
-                elif "who" in batch.qn_tokens or "Who" in batch.qn_tokens[ex_idx]:
+                elif "who" in batch.qn_tokens[ex_idx] or "Who" in batch.qn_tokens[ex_idx]:
                     question_type = "who"
-                elif "Do" in batch.qn_tokens or "do" in batch.qn_tokens[ex_idx]:
+                elif "Do" in batch.qn_tokens[ex_idx] or "do" in batch.qn_tokens[ex_idx]:
                     question_type = "do"
+                elif "which" in batch.qn_tokens[ex_idx] or "Which" in batch.qn_tokens[ex_idx]:
+                    question_type = "which"
                 else:
                     question_type = "other"
                 list_of_data_tuples.append((f1, em, question_type, true_answer_length, pred_answer_length))
